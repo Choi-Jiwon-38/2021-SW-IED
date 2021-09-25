75aeb0f6a4bbb1cc8a70b49e6cca7164aa1a8e79
최지원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2021-09-25 17:10:46 +0900

    add 05_practice_2

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,17 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+}
+void loop() {
+  digitalWrite(PIN_LED, 1);
+  delay(1000);
+  for (int i = 0; i < 10; i++) {
+    int toggle = i % 2;
+    digitalWrite(PIN_LED, toggle);
+    delay(100);
+  }
+  while(1){digitalWrite(PIN_LED, 0);} // infinite loop
+}
