8208d0d6d5ea04f85edbaecef3737ade45e59bc3
최지원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2021-09-28 15:22:50 +0900

    Correctly changing the signal value of PIN_LED

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -6,12 +6,12 @@ void setup() {
   Serial.begin(115200); // Initialize serial port
 }
 void loop() {
-  digitalWrite(PIN_LED, 1);
+  digitalWrite(PIN_LED, 0);
   delay(1000);
   for (int i = 0; i < 10; i++) {
-    int toggle = i % 2;
+    int toggle = 1 - (i % 2);
     digitalWrite(PIN_LED, toggle);
     delay(100);
   }
-  while(1){digitalWrite(PIN_LED, 0);} // infinite loop
+  while(1){digitalWrite(PIN_LED, 1);} // infinite loop
 }
